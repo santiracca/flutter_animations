269dcd4f76513afda6e552d518df3a10bf833fff
santiracca <santiagoracca7@gmail.com>
2020-06-30 19:38:16 -0400

    Finish Cat in Box Animation

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:animation/src/widgets/cat.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class Home extends StatefulWidget {
   @override
@@ -9,23 +10,45 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> with TickerProviderStateMixin {
   Animation<double> catAnimation;
   AnimationController catController;
+  Animation<double> boxAnimation;
+  AnimationController boxController;
 
   @override
   void initState() {
     super.initState();
+    boxController =
+        AnimationController(duration: Duration(milliseconds: 300), vsync: this);
+
     catController = AnimationController(
-      duration: Duration(seconds: 1),
+      duration: Duration(milliseconds: 500),
       vsync: this,
     );
-    catAnimation = Tween(begin: 0.0, end: 100.0).animate(
+    catAnimation = Tween(begin: -35.0, end: -80.0).animate(
       CurvedAnimation(parent: catController, curve: Curves.easeIn),
     );
+    boxAnimation =
+        Tween(begin: pi * 0.6, end: pi * 0.65).animate(CurvedAnimation(
+      parent: boxController,
+      curve: Curves.easeInOut,
+    ));
+    boxAnimation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        boxController.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        boxController.forward();
+      }
+    });
+    boxController.forward();
   }
 
   void onTap() {
-    catController.isCompleted
-        ? catController.reverse()
-        : catController.forward();
+    if (catController.isCompleted) {
+      catController.reverse();
+      boxController.forward();
+    } else if (catController.isDismissed) {
+      catController.forward();
+      boxController.stop();
+    }
   }
 
   @override
@@ -37,9 +60,11 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
       body: GestureDetector(
         onTap: onTap,
         child: Center(
-          child: Stack(children: [
+          child: Stack(overflow: Overflow.visible, children: [
             buildCatAnimation(),
             buildBox(),
+            buildleftFlap(),
+            buildRightFlap()
           ]),
         ),
       ),
@@ -50,9 +75,11 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
     return AnimatedBuilder(
       animation: catAnimation,
       builder: (context, child) {
-        return Container(
+        return Positioned(
           child: child,
-          margin: EdgeInsets.only(top: catAnimation.value),
+          top: catAnimation.value,
+          right: 0.0,
+          left: 0.0,
         );
       },
       child: Cat(),
@@ -64,6 +91,50 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
       height: 200,
       width: 200,
       color: Colors.brown,
+    );
+  }
+
+  Widget buildleftFlap() {
+    return Positioned(
+      left: 4,
+      top: 8,
+      child: AnimatedBuilder(
+        animation: boxAnimation,
+        child: Container(
+          height: 10,
+          width: 125,
+          color: Colors.brown,
+        ),
+        builder: (context, child) {
+          return Transform.rotate(
+            child: child,
+            angle: boxAnimation.value,
+            alignment: Alignment.topLeft,
+          );
+        },
+      ),
+    );
+  }
+
+  Widget buildRightFlap() {
+    return Positioned(
+      right: 4,
+      top: 8,
+      child: AnimatedBuilder(
+        animation: boxAnimation,
+        child: Container(
+          height: 10,
+          width: 125,
+          color: Colors.brown,
+        ),
+        builder: (context, child) {
+          return Transform.rotate(
+            child: child,
+            angle: -boxAnimation.value,
+            alignment: Alignment.topRight,
+          );
+        },
+      ),
     );
   }
 }
